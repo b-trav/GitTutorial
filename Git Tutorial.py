b761bf1feec22de1df69b386ad35767a97feaf10
Ben Travaglione <ben@travaglione.com>
2018-03-02 12:22:40 +0800

    Initial commit of this repository. Committing the Jupyter notebook containing a tutorial about git.

---
+++ Git Tutorial.py
@@ -0,0 +1,118 @@
+
+# coding: utf-8
+
+# # Git Tutorial
+# 
+# 2/3/2018
+
+# This notebook contains a very brief introduction to Git.
+# 
+# Git is a very useful and powerful version control system
+# 
+# Git is **NOT** very user-friendly
+# 
+# There are a number of websites and programs that attempt to make git more user friendly
+
+# ## Resources
+
+# - https://try.github.io/ - cool site to introduce you to git
+# - https://git-scm.com/doc - Official documentation
+# - https://git-scm.com/book/en/v2 - Free Git Book
+
+# ## Installing git
+
+# Go to https://git-scm.com/ and install Git for your particular operating system
+
+# ## Setting up git
+
+# Open up a terminal window - this will depend on your operating system.
+#  - Windows: start GitBash
+#  - Linux: open a terminal
+#  - OS X: open a terminal???
+#  - ChromsOS : In developer mode, type CLT-ALT-T
+
+# In[ ]:
+
+
+git 
+
+
+# ## Github
+
+# [Github](https://github.com) is a website that allows you to easily create public git repositories. Students are also able to create private git repositories.
+# 
+# You should sign up to github when you have the chance, however you do not need to for the next step.
+# 
+# Clone the repository containing this document:
+# 
+# ```
+# git clone https://github.com/b-trav/GitTutorial.git
+# ```
+
+# ## Repository
+
+# - A repository is a store of all the versions of all the files in a git project (directory).
+# - A repository can either be a _working_ repository, or a _bare_ repository.
+# - Working repositories have all your files in them, but they also have a sub-directory called `.git` which contains all the versions of your work.
+# - A bare repository is simply the .git folder on its own - normally called `ProjectName.git`
+
+# ## Cloning a repository
+
+# ```
+# git clone https://github.com/b-trav/GitTutorial.git
+# ```
+
+# ## Creating a repository
+
+# Rather than cloning a repository, you can make your current directory into a git project by simply typing:
+# ```
+# git init
+# ```
+
+# ## Checking the status of a repository
+
+# In[ ]:
+
+
+
+
+
+# ## Adding files
+
+# In[ ]:
+
+
+
+
+
+# Committing files
+
+# In[ ]:
+
+
+
+
+
+# ## Pushing files
+
+# In[ ]:
+
+
+
+
+
+# ## Pulling files
+
+# In[ ]:
+
+
+
+
+
+# ## Branches, merging and much more
+
+# In[ ]:
+
+
+
+
